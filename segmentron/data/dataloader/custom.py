9d1715b0ed5ee0bd19f3719f41da30ccabc41f52
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2020-10-15 15:37:51 +0800

    Update custom.py

--- segmentron/data/dataloader/custom.py
+++ segmentron/data/dataloader/custom.py
@@ -39,14 +39,14 @@ class CustomSegmentation(SegmentationDataset):
     """
     NUM_CLASS = 1
     TRAIN_PATH = '/kaggle/input/circle-finder-marathon-challenge-train-data/train'
-    MASKS_PATH = 'masks'
+    MASK_PATH = 'masks'
 
-    def __init__(self, root=TRAIN_PATH, split='train', mode=None, transform=None, **kwargs):
+    def __init__(self, root=this.TRAIN_PATH, split='train', mode=None, transform=None, **kwargs):
         super(CustomSegmentation, self).__init__(root, split, mode, transform, **kwargs)
         self.root = root
         assert os.path.exists(self.root)
         _get_masks(self.root)
-        self.images, self.mask_paths = _get_dataset_pairs(self.root, MASKS_PATH, self.split)
+        self.images, self.mask_paths = _get_dataset_pairs(self.root, this.MASK_PATH, self.split)
         assert (len(self.images) == len(self.mask_paths))
         if len(self.images) == 0:
             raise RuntimeError("Found 0 images in subfolders of:" + root + "\n")
