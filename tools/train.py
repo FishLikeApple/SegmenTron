efe33ee3b1d209d9c1ac9b226639c3d7c1a10966
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2020-10-16 14:17:26 +0800

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -134,6 +134,8 @@ class Trainer(object):
             targets = targets.to(self.device)
 
             outputs = self.model(images)
+            print(outputs.shape)
+            print(targets.shape)
             loss_dict = self.criterion(outputs, targets)
 
             losses = sum(loss for loss in loss_dict.values())
