e60e29e6267ba115d29163388e2ed1a0fe030271
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2020-10-21 01:15:46 +0800

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -141,11 +141,11 @@ class Trainer(object):
             print(image_arrays.shape)
             print(target_arrays.shape)
             for i in range(image_arrays.shape[0]):
-                ay = (np.swapaxes(image_arrays[i], 0, 2)*255).astype(np.uint8)
+                ay = (np.swapaxes(image_arrays[i], 0, 2)).astype(np.uint8)
                 im = Image.fromarray(ay)
                 im.save("./image{}.jpeg".format(i))
                 ay = np.expand_dims(target_arrays[i], 2)
-                ay = (np.concatenate([ay, ay, ay], 2)*255).astype(np.uint8)
+                ay = (np.concatenate([ay, ay, ay], 2)).astype(np.uint8)
                 im = Image.fromarray(ay)
                 im.save("./mask{}.jpeg".format(i))
                 os.system("rm -rf masks")
