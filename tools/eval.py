2213666b471a591b881e204516b5bb7cf386c33a
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2020-10-26 16:38:23 +0800

    Update eval.py

--- tools/eval.py
+++ tools/eval.py
@@ -32,11 +32,11 @@ class Evaluator(object):
         # image transform
         input_transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(cfg.DATASET.MEAN, cfg.DATASET.STD),
+            #transforms.Normalize(cfg.DATASET.MEAN, cfg.DATASET.STD),
         ])
 
         # dataset and dataloader
-        val_dataset = get_segmentation_dataset(cfg.DATASET.NAME, split='val', mode='testval', transform=input_transform)
+        val_dataset = get_segmentation_dataset(cfg.DATASET.NAME, split='trainval', mode='test', transform=input_transform)
         val_sampler = make_data_sampler(val_dataset, False, args.distributed)
         val_batch_sampler = make_batch_data_sampler(val_sampler, images_per_batch=cfg.TEST.BATCH_SIZE, drop_last=False)
         self.val_loader = data.DataLoader(dataset=val_dataset,
