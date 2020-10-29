91386d40a33ddbec3962f0d046548ee0a9a27363
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2020-10-29 16:51:30 +0800

    Update seg_data_base.py

--- segmentron/data/dataloader/seg_data_base.py
+++ segmentron/data/dataloader/seg_data_base.py
@@ -78,6 +78,7 @@ class SegmentationDataset(object):
     def mask_reversion_transform(self, mask, target_size):
         w, h = mask.size
         assert  (w == h)
+        print(target_size)
         if target_size[0] > target_size[1]:
             ow = target_size[0]
             oh = target_size[0]
